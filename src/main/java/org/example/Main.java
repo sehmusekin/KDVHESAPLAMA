76734ba8171dd77d78c16b1c0a3660c5cdb78d85
ŞEHMUS EKİN <146784285+sehmusekin@users.noreply.github.com>
2024-05-30 20:30:54 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,38 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double tutar;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("KDV Hesaplama");
+
+        System.out.print("ÜCRET TUTARI GİRİNİZ : ");
+        tutar = inp.nextDouble();
+
+
+        double kdvOrani;
+        if(tutar >1000){
+            kdvOrani=0.08;
+        } else {
+            kdvOrani=0.18;
+
+        }
+
+
+        double kdvtutar=(tutar*kdvOrani);
+        double kdvliTutar=(tutar +kdvtutar);
+
+        System.out.println("KDV'siz Tutar :" +tutar);
+        System.out.println("KDV Oranı :"+kdvOrani );
+        System.out.println("KDV Tutarı :" +kdvliTutar);
+        System.out.println("KDV'li Tutar :" +kdvliTutar);
+
+
+
     }
 }
